1073ff993c34fdb6d53ae3b17f3f8fd94ef0b708
merlin1031 <rene.triebenstein@me.com>
2020-06-14 00:01:11 +0200

    Filmliste erweitert

--- FilmListe.py
+++ FilmListe.py
@@ -1,8 +1,23 @@
 import os
+from datetime import datetime as DateTime
 
+# Pfad zu den Filmen
 FilmPfad = "/Volumes/osx home/kodi2020"
 
+# Liste aller Unterverzeichnissen erstellen
 FilmListe = os.listdir(FilmPfad)
-print (FilmListe)
-print (FilmListe.__sizeof__())
+
+# Apple Verzeichnis-Zeugs entfernen
+FilmListe.remove(".DS_Store")
+
+# Listen Kopf schreiben
 print ("Anzahl Filme: " + str(len(FilmListe)))
+print ("Uhrzeit: " + DateTime.now().strftime('%H:%M:%S'))
+print ("-------------------------------")
+
+# alle Filme in der Liste zeilenweise ausgeben
+i = 1
+for FilmName in FilmListe:
+    print(str(i) + ": " + FilmName)
+    i = i + 1
+
